2d818298f823bd4b6824c7980e7455e02e374afb
Anshul Kapoor <anshulkapoor018@gmail.com>
2020-09-14 17:37:18 -0400

    Page 5 exercise done

---
+++ A01/A01_Deliverable_1_Group8.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+"""
+A01_Deliverable_1_Group8.py
+"""
+# Step 1 -> Getting Started with NLTK
+# import nltk
+# nltk.download()
+
+from nltk.book import *
+# print(text1)
+
+# print("\n")
+# # Step 2 -> Searching Text
+# print("******* Concordance for Word \"monstrous\" *******")
+# print(text1.concordance("monstrous"))
+print("\n")
+
+# Your Turn Page No.5
+
+print("******* Concordance for Word \"affection\" *******")
+print(text2.concordance("affection"))
+print("\n")
+print("******* Concordance for Word \"lived\" *******")
+print(text3.concordance("lived"))
+print("\n")
+print("******* Concordance for Word \"nation\" *******")
+print(text4.concordance("nation"))
+print("\n")
+print("******* Concordance for Word \"Pictures\" *******")
+print(text1.concordance("Pictures"))
+print("\n")
+print("******* Concordance for Word \"god\" *******")
+print(text4.concordance("god"))
+print("\n")
+print("******* Concordance for Word \"lol\" *******")
+print(text5.concordance("lol"))
+print("\n")
+print("******* Concordance for Word \"Journal\" *******")
+print(text7.concordance("Journal"))
+print("\n")
+print("******* Concordance for Word \"ur\" *******")
+print(text5.concordance("ur"))
